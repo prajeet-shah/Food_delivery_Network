3000583d194c579db4f7f938e7b89c053d33577c
Prajeet Shah <prajeetshah93@gmail.com>
2024-10-20 14:03:17 +0545

    Episode 1

---
+++ App.js
@@ -0,0 +1,15 @@
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child1" }, [
+    React.createElement("h1", { id: "h1" }, "I am h1 tag"),
+    React.createElement("h2", { id: "h2" }, "I am h2 tag"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", { id: "h1" }, "I am h1 tag"),
+    React.createElement("h2", { id: "h2" }, "I am h2 tag"),
+  ]),
+]);
+
+// ReactElement(Object) ==> HTML(Browser Understands)
+// console.log(heading); //object
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
