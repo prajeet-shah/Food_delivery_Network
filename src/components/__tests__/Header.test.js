ba80fa3b5db6964044577571734ce885239158f9
Prajeet Shah <prajeetshah93@gmail.com>
2025-01-06 12:23:33 +0545

    chnage in header.test.js

--- src/components/__tests__/Header.test.js
+++ src/components/__tests__/Header.test.js
@@ -15,8 +15,40 @@ it("should load Header component with login button", () => {
   );
 
   // quering
-  const button = screen.getByRole("button");
+  const button = screen.getByRole("button", { name: "Login" });
 
   //Assertion
   expect(button).toBeInTheDocument();
+});
+
+it("should lead Header component with cart with 0 items", () => {
+  render(
+    <BrowserRouter>
+      <Provider store={appStore}>
+        <Header />
+      </Provider>
+    </BrowserRouter>
+  );
+
+  // quering
+  const cartItems = screen.getByText("Cart - (0 items)");
+
+  //Assertion
+  expect(cartItems).toBeInTheDocument();
+});
+
+it("should lead Header component with cart items ", () => {
+  render(
+    <BrowserRouter>
+      <Provider store={appStore}>
+        <Header />
+      </Provider>
+    </BrowserRouter>
+  );
+
+  // quering
+  const cartItems = screen.getByText(/Cart/);
+
+  //Assertion
+  expect(cartItems).toBeInTheDocument();
 });
